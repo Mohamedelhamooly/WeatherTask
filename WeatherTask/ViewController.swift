08397682bdea27d90f37fa15c41840a4ff6ecbc5
mohamed elhamoly <melhamoly@codepro.sa>
2020-12-07 23:04:03 +0200

    Initial Commit

---
+++ WeatherTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTask
+//
+//  Created by Mohamed Elhamoly on 12/7/20.
+//  Copyright © 2020 mohamed Elhamoly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
